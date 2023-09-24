2ebb9491f5d01d4bdaf1fd7b87c9b1711c992690
Pan-Sareerawan <pansareerawan@gmail.com>
2023-09-24 12:08:18 +0700

    project2

---
+++ vue-project2/src/components/myCard.vue
@@ -0,0 +1,38 @@
+<template>
+  
+<div class="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr3qBVX4XIA8zq3LpBn64zAuOt9_IZ7_H5uA&usqp=CAU" alt="Avatar" style="width:100%">
+  <div class="container">
+    <h4><b>John Doe</b></h4> 
+    <p>Architect & Engineer</p> 
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+data(){
+  return
+}
+}
+</script>
+
+<style scoped>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 30%;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.container {
+  padding: 2px 16px;
+}
+/*img{
+  width: 50px;
+  height: 150px;
+}*/
+</style>
